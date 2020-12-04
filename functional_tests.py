ea7ea294cb532b2e82b2d67595f2f21185ec895b
mivicker <vickers.mike@gmail.com>
2020-12-03 22:43:46 -0500

    Functional test checks if we can input a to-do item"

--- functional_tests.py
+++ functional_tests.py
@@ -17,16 +17,37 @@ class NewVisitorTest(unittest.TestCase):
 		#she glances over and does not notice that the title says to-do, but it just feels right.
 
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test')
+		
+		#Maybe she reads the headline for once
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self. assertIn('To-do', header_text)
 
 		#She is prompted to enter a to do item, to give her that little pang of stress from our application
 		#allways pushing our customers to be better, be more alpha.
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+
 		#She enters 'revise resume' because that's what us in this capitalist hell hole have no excuse
 		#not to be doing aside from sheer sloth 
 
+		inputbox.send_keys('revise resume')
+
 		#When she hits enter, the page updates and displays her knew item urgently
 
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1. revise resume' for row in rows)
+			)
+
+		self.fail('Finish the test!')
 		#This leads to another item 'email resume to someone who doesn't care' which is 
 		#both a task and the admittion of failure.
 
