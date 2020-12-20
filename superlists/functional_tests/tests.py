b331e36e9c35724c51c4d34cc3826d8e810e48ff
mivicker <vickers.mike@gmail.com>
2020-12-19 21:58:40 -0500

    first steps in FT for style

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -74,6 +74,30 @@ class NewVisitorTest(LiveServerTestCase):
 		#The page updates, showing both, and edith wonders if this is worth it at all
 		#Will she keep this pointless habit?
 
+	def test_layout_and_styling(self):
+		#Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		#She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta = 10
+			)
+
+		#She sees that new list is also nicely centered
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1. testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta = 10
+			)		
+
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		#Edith starts a new to-do list
 		self.browser.get(self.live_server_url)
