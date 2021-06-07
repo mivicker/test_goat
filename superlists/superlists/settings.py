7372950a4ff4d8222484ef36583cd8d609d7ed85
mike <vickers.mike@gmail.com>
2021-06-06 22:42:44 -0400

    added environ conditional to settings

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ovqz*=zwu@y5h!5@5eugd@26d3a+j=@hv&y2b0m_8-^gk90+&f'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 
-ALLOWED_HOSTS = ['*']
+else:
+    DEBUG = True
+    SECRET_KEY = 'ovqz*=zwu@y5h!5@5eugd@26d3a+j=@hv&y2b0m_8-^gk90+&f'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
